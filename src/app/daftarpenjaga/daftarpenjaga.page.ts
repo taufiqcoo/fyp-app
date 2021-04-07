25046f0387021b1bb9d11f158f7a770264aeb664
taufiqcoo <81658815+taufiqcoo@users.noreply.github.com>
2021-04-07 13:16:09 +0800

    fyp

---
+++ src/app/daftarpenjaga/daftarpenjaga.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daftarpenjaga',
+  templateUrl: './daftarpenjaga.page.html',
+  styleUrls: ['./daftarpenjaga.page.scss'],
+})
+export class DaftarpenjagaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
